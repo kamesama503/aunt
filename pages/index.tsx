a6a1181b5d8fbe1d5e152acf4552e7bf5b6ec352
Yu.Jiayu <yu.jiayu@tci-cn.co.jp>
2021-06-14 14:44:15 +0900

    利用数据库查询

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,67 @@
-import fs from "fs";
 import moment from "moment";
 import React, { useState } from "react";
 import { useAllAuntDates } from "../graphql/api";
 
-// 读取日期文件
-const getDate = async () => {
-  return JSON.parse(fs.readFileSync("./public/yueyueData.json", "utf8"));
-};
+export default function Container() {
+  const [name, setName] = useState(null);
+  const [newDate, setNewDate] = useState(null);
 
-// 计算下次时间
-const calcNextDate = async () => {
-  const data = await getDate();
-  const length = data.length;
-  if (length > 1) {
-    let total = 0;
-    for (let i = 1; i < length; i++) {
-      const diff = moment(data[i]).diff(moment(data[i - 1]), "days");
-      total += diff;
-    }
-    const avgDiff = Math.round(total / (length - 1));
-    const nextDate = moment(data[length - 1]).add(avgDiff, "days");
-    return nextDate.format("YYYY-MM-DD");
-  } else {
-    return "数据不足，无法计算";
-  }
-};
-
-export default function Container({ nextDate }) {
-  const [value, setValue] = useState(null);
-
+  // 读取所有数据
   const { data } = useAllAuntDates();
-  if (data) {
-    console.log(data);
-  }
+  const dates = data?.allAuntDates?.data;
+
+  // 获取该用户的日期数据
+  const userDates = dates
+    ?.filter((date) => date.name == name)
+    .map((date) => date.date);
+  console.log("userDates", userDates);
+
+  // 计算下次时间
+  const calcNextDate = () => {
+    const length = userDates.length;
+    if (length > 1) {
+      let total = 0;
+      for (let i = 1; i < length; i++) {
+        console.log("userDates[i]", userDates[i]);
+        const diff = moment(userDates[i]).diff(
+          moment(userDates[i - 1]),
+          "days"
+        );
+        total += diff;
+      }
+      const avgDiff = Math.round(total / (length - 1));
+      const nextDate = moment(userDates[length - 1]).add(avgDiff, "days");
+      console.log(nextDate.format("YYYY-MM-DD"));
+      return nextDate.format("YYYY-MM-DD");
+    } else {
+      return "数据不足，无法计算";
+    }
+  };
 
   return (
     <>
-      <div>下次降临：{nextDate}</div>
+      <div>
+        用户名：
+        <input value={name} onChange={(event) => setName(event.target.value)} />
+      </div>
+      <div>下次降临：{calcNextDate()}</div>
       <div>
         新日期：
         <input
           type="date"
-          value={value}
-          onChange={(event) => setValue(event.target.value)}
+          value={newDate}
+          onChange={(event) => setNewDate(event.target.value)}
         />
         <button
           type="button"
           onClick={() => {
-            console.log("value", value);
+            console.log("value", newDate);
           }}
         >
           添加
         </button>
       </div>
+      <div>历史数据：{userDates}</div>
     </>
   );
-}
-
-export async function getStaticProps() {
-  const nextDate = await calcNextDate();
-  return {
-    props: {
-      nextDate,
-    },
-  };
 }
